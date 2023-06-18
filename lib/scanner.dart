7ecf662d268b5ed421472cce0c6e73f903ceb2fb
Manas Malla <manas@theananta.in>
2023-06-18 22:44:34 +0530

    stash ML model and implement ui
    Signed-off-by: Manas Malla <manas@theananta.in>

--- lib/scanner.dart
+++ lib/scanner.dart
@@ -28,6 +28,7 @@ class ScannerPage extends StatefulWidget {
 enum ScannerState { CAPTURE, WARNING, ERROR }
 
 class _ScannerPageState extends State<ScannerPage> {
+  List<XFile> images = [];
   var state = ScannerState.CAPTURE;
 
   Future<void> _initializeCamera() async {
@@ -56,7 +57,6 @@ class _ScannerPageState extends State<ScannerPage> {
 
   @override
   Widget build(BuildContext context) {
-    // showWarning();
     return Scaffold(
       body: Stack(
         children: [
@@ -69,7 +69,9 @@ class _ScannerPageState extends State<ScannerPage> {
           FutureBuilder(
               future: _initializeCamera(),
               builder: (context, _) {
-                return CameraPreview(_cameraController!);
+                return SizedBox(
+                    height: double.infinity,
+                    child: CameraPreview(_cameraController!));
               }),
           AnimatedSwitcher(
             duration: Duration(milliseconds: 300),
@@ -100,51 +102,73 @@ class _ScannerPageState extends State<ScannerPage> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Stack(
-                      clipBehavior: Clip.none,
-                      children: [
-                        Transform.translate(
-                          offset: const Offset(8, -8),
-                          child: Transform.rotate(
-                            angle: -20,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(16),
-                              child: Image.asset(
-                                "assets/scanner.jpg",
-                                fit: BoxFit.cover,
-                                width: 64,
-                                height: 64,
-                                color: const Color(0x60981F2B),
-                                colorBlendMode: BlendMode.multiply,
+                    images.isNotEmpty
+                        ? Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              Transform.translate(
+                                offset: const Offset(8, -8),
+                                child: Transform.rotate(
+                                  angle: -20,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(16),
+                                    child: FutureBuilder(
+                                        future: images.length > 1
+                                            ? images.reversed
+                                                .take(2)
+                                                .last
+                                                .readAsBytes()
+                                            : images.last.readAsBytes(),
+                                        builder: (context, snapshot) {
+                                          return snapshot.data != null
+                                              ? Image.memory(
+                                                  snapshot.data!,
+                                                  fit: BoxFit.cover,
+                                                  width: 64,
+                                                  height: 64,
+                                                  color:
+                                                      const Color(0x60981F2B),
+                                                  colorBlendMode:
+                                                      BlendMode.multiply,
+                                                )
+                                              : CircularProgressIndicator();
+                                        }),
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                        ),
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(16),
-                          child: Image.asset(
-                            "assets/scanner.jpg",
-                            fit: BoxFit.cover,
-                            width: 64,
-                            height: 64,
-                          ),
-                        ),
-                        Positioned(
-                          bottom: -6,
-                          right: -6,
-                          child: Container(
-                            padding: const EdgeInsets.all(8.0),
-                            decoration: const BoxDecoration(
-                                color: Color(0xFF981F2B),
-                                shape: BoxShape.circle),
-                            child: Text(
-                              "4",
-                              style: GoogleFonts.poppins(color: Colors.white),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(16),
+                                child: FutureBuilder(
+                                    future: images.last.readAsBytes(),
+                                    builder: (context, snapshot) {
+                                      return snapshot.data != null
+                                          ? Image.memory(
+                                              snapshot.data!,
+                                              fit: BoxFit.cover,
+                                              width: 64,
+                                              height: 64,
+                                            )
+                                          : CircularProgressIndicator();
+                                    }),
+                              ),
+                              Positioned(
+                                bottom: -6,
+                                right: -6,
+                                child: Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  decoration: const BoxDecoration(
+                                      color: Color(0xFF981F2B),
+                                      shape: BoxShape.circle),
+                                  child: Text(
+                                    images.length.toString(),
+                                    style: GoogleFonts.poppins(
+                                        color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        : SizedBox(),
                     InkWell(
                       onTap: () async {
                         String name =
@@ -153,26 +177,28 @@ class _ScannerPageState extends State<ScannerPage> {
                             await getApplicationDocumentsDirectory();
                         XFile capture = await _cameraController!.takePicture();
                         await capture.saveTo(directory.path + '$name.jpeg');
+                        images.add(capture);
+                        setState(() {});
                         // final interpreter = Tflite.loadModel(
                         //     model: 'assets/model.tflite',
                         //     labels: 'assets/labels.txt');
                         // final result = await Tflite.runModelOnImage(
                         //     path: directory.path + '$name.jpeg');
                         // print(result);
-                        final options = LocalLabelerOptions(
-                            modelPath: 'flutter_assets/assets/model.tflite');
-                        final imagelabeler = ImageLabeler(options: options);
-                        final prediction = imagelabeler.processImage(
-                            InputImage.fromBytes(
-                                bytes: await capture.readAsBytes(),
-                                metadata: InputImageMetadata(
-                                    size: Size.fromWidth(
-                                        MediaQuery.of(context).size.width),
-                                    rotation: InputImageRotation.rotation0deg,
-                                    format: InputImageFormat.bgra8888,
-                                    bytesPerRow:
-                                        (await capture.readAsBytes()).length)));
-                        print(prediction);
+                        // final options = LocalLabelerOptions(
+                        //     modelPath: 'flutter_assets/assets/model.tflite');
+                        // final imagelabeler = ImageLabeler(options: options);
+                        // final prediction = imagelabeler.processImage(
+                        //     InputImage.fromBytes(
+                        //         bytes: await capture.readAsBytes(),
+                        //         metadata: InputImageMetadata(
+                        //             size: Size.fromWidth(
+                        //                 MediaQuery.of(context).size.width),
+                        //             rotation: InputImageRotation.rotation0deg,
+                        //             format: InputImageFormat.bgra8888,
+                        //             bytesPerRow:
+                        //                 (await capture.readAsBytes()).length)));
+                        // print(prediction);
                       },
                       child: Container(
                         width: 64,
@@ -192,16 +218,21 @@ class _ScannerPageState extends State<ScannerPage> {
                         ),
                       ),
                     ),
-                    Container(
-                      width: 56,
-                      height: 56,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white.withOpacity(0.5),
-                      ),
-                      child: const Icon(
-                        FeatherIcons.zap,
-                        color: Color(0x50202023),
+                    InkWell(
+                      onTap: () {
+                        //Implement the syncfusion image to pdf
+                      },
+                      child: Container(
+                        width: 56,
+                        height: 56,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: const Icon(
+                          FeatherIcons.check,
+                          color: Color(0x80202023),
+                        ),
                       ),
                     ),
                   ],
